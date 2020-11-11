0a3846729b5c1888d09bb1a0cb977596f55a3587
crimeprince4 <73919224+crimeprince4@users.noreply.github.com>
2020-11-11 22:44:47 +0700

    Add files via upload
    DB hiện tại

---
+++ du_an_1 (4).sql
@@ -0,0 +1,307 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 11, 2020 lúc 04:42 PM
+-- Phiên bản máy phục vụ: 10.4.14-MariaDB
+-- Phiên bản PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `du_an_1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `dang_tuyen`
+--
+
+CREATE TABLE `dang_tuyen` (
+  `id_dt` int(11) NOT NULL,
+  `anh` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tieu_de` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `noi_dung` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `yeu_cau` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sl_sv_can` int(11) NOT NULL,
+  `sl_sv_dk` int(11) NOT NULL,
+  `id_dn` int(11) NOT NULL,
+  `id_nganh` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `doanh_nghiep`
+--
+
+CREATE TABLE `doanh_nghiep` (
+  `id_dn` int(11) NOT NULL,
+  `id_user` int(12) NOT NULL,
+  `ten_dn` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `anh` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `dia_chi` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sdt` int(11) NOT NULL,
+  `an_hien` tinyint(1) DEFAULT 1,
+  `stt` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nganh`
+--
+
+CREATE TABLE `nganh` (
+  `id_nganh` int(11) NOT NULL,
+  `ten_nganh` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `an_hien` tinyint(1) DEFAULT NULL,
+  `id_nn` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nhom_nganh`
+--
+
+CREATE TABLE `nhom_nganh` (
+  `id_nn` int(11) NOT NULL,
+  `ten_nn` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `an_hien` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `phieu_dk`
+--
+
+CREATE TABLE `phieu_dk` (
+  `id_phieu` int(11) NOT NULL,
+  `id_sv` int(11) NOT NULL,
+  `ngay_dk` date NOT NULL,
+  `nguyen_vong` varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id_dt` int(11) NOT NULL,
+  `trang_thai` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `phieu_dk_`
+--
+
+CREATE TABLE `phieu_dk_` (
+  `id_phieu_` int(11) NOT NULL,
+  `ten_dn` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_sv` int(11) NOT NULL,
+  `dia_chi` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sdt` int(11) NOT NULL,
+  `trang_thai` tinyint(1) NOT NULL COMMENT '0: chưa xử lí \r\n1 đã duyet'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `sinh_vien`
+--
+
+CREATE TABLE `sinh_vien` (
+  `id_sv` int(11) NOT NULL,
+  `id_user` int(12) NOT NULL,
+  `mssv` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_nganh` int(11) NOT NULL,
+  `ho_ten` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gioi_tinh` tinyint(1) NOT NULL,
+  `anh` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sdt` int(11) NOT NULL,
+  `trang_thai` tinyint(1) DEFAULT 0 COMMENT '0: chưa có nơi thực tập\r\n1: có nơi thực tập',
+  `ket_qua` tinyint(1) NOT NULL COMMENT '0: chưa có\r\n1: đạt\r\n2: không đạt',
+  `ghi_chu` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(12) NOT NULL,
+  `user` varchar(50) NOT NULL,
+  `pass` varchar(30) NOT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `chuc_vu` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `dang_tuyen`
+--
+ALTER TABLE `dang_tuyen`
+  ADD PRIMARY KEY (`id_dt`),
+  ADD KEY `id_dn` (`id_dn`,`id_nganh`),
+  ADD KEY `id_nganh` (`id_nganh`);
+
+--
+-- Chỉ mục cho bảng `doanh_nghiep`
+--
+ALTER TABLE `doanh_nghiep`
+  ADD PRIMARY KEY (`id_dn`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Chỉ mục cho bảng `nganh`
+--
+ALTER TABLE `nganh`
+  ADD PRIMARY KEY (`id_nganh`),
+  ADD KEY `id_nn` (`id_nn`);
+
+--
+-- Chỉ mục cho bảng `nhom_nganh`
+--
+ALTER TABLE `nhom_nganh`
+  ADD PRIMARY KEY (`id_nn`);
+
+--
+-- Chỉ mục cho bảng `phieu_dk`
+--
+ALTER TABLE `phieu_dk`
+  ADD PRIMARY KEY (`id_phieu`),
+  ADD KEY `id_sv` (`id_sv`,`id_dt`),
+  ADD KEY `id_dt` (`id_dt`);
+
+--
+-- Chỉ mục cho bảng `phieu_dk_`
+--
+ALTER TABLE `phieu_dk_`
+  ADD PRIMARY KEY (`id_phieu_`),
+  ADD KEY `id_sv` (`id_sv`);
+
+--
+-- Chỉ mục cho bảng `sinh_vien`
+--
+ALTER TABLE `sinh_vien`
+  ADD PRIMARY KEY (`id_sv`),
+  ADD KEY `id_nganh` (`id_nganh`),
+  ADD KEY `id_user1` (`id_user`);
+
+--
+-- Chỉ mục cho bảng `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `dang_tuyen`
+--
+ALTER TABLE `dang_tuyen`
+  MODIFY `id_dt` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT cho bảng `doanh_nghiep`
+--
+ALTER TABLE `doanh_nghiep`
+  MODIFY `id_dn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT cho bảng `nganh`
+--
+ALTER TABLE `nganh`
+  MODIFY `id_nganh` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT cho bảng `nhom_nganh`
+--
+ALTER TABLE `nhom_nganh`
+  MODIFY `id_nn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT cho bảng `phieu_dk`
+--
+ALTER TABLE `phieu_dk`
+  MODIFY `id_phieu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT cho bảng `phieu_dk_`
+--
+ALTER TABLE `phieu_dk_`
+  MODIFY `id_phieu_` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT cho bảng `sinh_vien`
+--
+ALTER TABLE `sinh_vien`
+  MODIFY `id_sv` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT cho bảng `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(12) NOT NULL AUTO_INCREMENT;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `dang_tuyen`
+--
+ALTER TABLE `dang_tuyen`
+  ADD CONSTRAINT `dang_tuyen_ibfk_1` FOREIGN KEY (`id_dn`) REFERENCES `doanh_nghiep` (`id_dn`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `dang_tuyen_ibfk_2` FOREIGN KEY (`id_nganh`) REFERENCES `nganh` (`id_nganh`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `doanh_nghiep`
+--
+ALTER TABLE `doanh_nghiep`
+  ADD CONSTRAINT `id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `nganh`
+--
+ALTER TABLE `nganh`
+  ADD CONSTRAINT `nganh_ibfk_1` FOREIGN KEY (`id_nn`) REFERENCES `nhom_nganh` (`id_nn`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `phieu_dk`
+--
+ALTER TABLE `phieu_dk`
+  ADD CONSTRAINT `phieu_dk_ibfk_1` FOREIGN KEY (`id_phieu`) REFERENCES `sinh_vien` (`id_sv`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `phieu_dk_ibfk_2` FOREIGN KEY (`id_dt`) REFERENCES `dang_tuyen` (`id_dt`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `phieu_dk_`
+--
+ALTER TABLE `phieu_dk_`
+  ADD CONSTRAINT `phieu_dk__ibfk_1` FOREIGN KEY (`id_sv`) REFERENCES `sinh_vien` (`id_sv`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Các ràng buộc cho bảng `sinh_vien`
+--
+ALTER TABLE `sinh_vien`
+  ADD CONSTRAINT `id_user1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sinh_vien_ibfk_1` FOREIGN KEY (`id_nganh`) REFERENCES `nganh` (`id_nganh`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
