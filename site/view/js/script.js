303f6cb96cb7363d8fabdaf7fd1cfe6f368a3c02
onlyitme <69088614+onlyitme@users.noreply.github.com>
2020-11-11 10:39:03 +0700

    update-overlay

--- site/view/js/script.js
+++ site/view/js/script.js
@@ -5,5 +5,11 @@
         $("#nav-togger").toggleClass("nav-togger_left");
         $("#overlay-nav").toggleClass("d-block");
         $(".container").toggleClass("container_left");
+
+        $("#overlay-nav").click(function(e) {
+            e.preventDefault();
+            $("#nav-togger").removeClass("nav-togger_left");
+            $("#overlay-nav").removeClass("d-block");
+        });
     });
 })(jQuery);
