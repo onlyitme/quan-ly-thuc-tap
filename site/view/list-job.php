e957b723980b07f1ec24a205c49f11cb68851d1b
crimeprince4 <73919224+crimeprince4@users.noreply.github.com>
2020-11-20 08:40:59 +0700

    show list job + đăng nhập

--- site/view/list-job.php
+++ site/view/list-job.php
@@ -1,3 +1,14 @@
+<script language="javascript">
+      function get_content(a)
+      { 
+        var html = document.getElementById(a).value;
+
+       alert(html);
+        
+      }
+      
+       
+    </script>
 <div id="list-job">
     <div class="row bg-light shadow-sm  py-3 ">
         <form action="" method="post" class="d-lg-flex col-lg-9">
@@ -17,113 +28,44 @@
     <div class="row detail-job shadow-sm ">
         <div class="col  p-lg-0">
             <div class="d-flex justify-content-between  border-bottom border-top py-2 px-3">
-                <h5>10.000 việc làm</h5>
+                <h5>Hiện Có <?php echo $coutdangtuyen; ?> Việc Làm</h5>
                 <select name="" id="" class="small text-gray border-0">
                     <option value="">Cập nhật</option>
                     <option value="">Mức lương</option>
                 </select>
             </div>
             <div class="detail-job_list d-flex flex-wrap small">
-                <button type="button" class="list-group-item list-group-item-action px-0">
-                    <div class="media">
-                        <img class="card-img-top col-3" src="http://placehold.it/300x200" alt="Card image cap ">
+                <?php
+                foreach($thongtindangtuyen as $thongtindangtuyen){
+                    $thongtindn= checkdoanhnghiepbyid($thongtindangtuyen['id_dn']);
+                    $thongtinnganh=checknganhbyid($thongtindangtuyen['id_nganh']);
+                ?>
+                <button type="button" class="list-group-item list-group-item-action px-0" value="<?=$thongtindangtuyen['id_dt']?>" id="<?=$thongtindangtuyen['id_dt']?>"  onclick="get_content(<?=$thongtindangtuyen['id_dt']?>)" >
+                    <div class="media" >
+                        <img class="card-img-top col-3" src="../uploads/<?=$thongtindangtuyen['anh']?>" onerror=this.src="http://placehold.it/300x200" alt="Card image cap ">
                         <div class="media-body text-gray text-left">
-                            <a href=""><h5 class="text-dark mt-0 ">Tên công việc</h5></a>
-                            <p class="font-weight-bold ">Tên công ty</p>
-                            <p class="text-info"><i class="fas fa-coins    "></i> Lương: 123</p>
-                            <p><i class="fas fa-map-marker-alt    "></i> Hà Nội</p>
-                            <p ><i class="fas fa-calendar-alt "></i> Hạn chót: 25/09/20</p>
+                            <a href=""><h5 class="text-dark mt-0 "><?=$thongtinnganh['ten_nganh']?></h5></a>
+                            <p class="font-weight-bold "><?=$thongtindn['ten_dn']?></p>
+                            <p class="text-info"><i class="fas fa-coins    "></i> Lương: <?=$thongtindangtuyen['luong']?></p>
+                            <p><i class="fas fa-map-marker-alt    "></i> <?=$thongtindangtuyen['vi_tri']?></p>
+                            <p ><i class="fas fa-calendar-alt "></i> Hạn chót: <?php $today = date("Y-m-d");
+                                                                                    $expire = $thongtindangtuyen['thoi_gian'];     
+                                                                                    $today_time = strtotime($today);
+                                                                                    $expire_time = strtotime($expire);
+                                                                                    if ($expire_time < $today_time){
+                                                                                        echo 'hết hạn nộp';
+                                                                                    }else echo $expire;
+                                                                                    ?></p>
                         </div>
                         </div>
                 </button>
-                <button type="button" class="list-group-item list-group-item-action px-0 ">
-                    <div class="media">
-                        <img class="card-img-top col-3" src="http://placehold.it/300x200" alt="Card image cap ">
-                        <div class="media-body text-gray text-left">
-                            <a href=""><h5 class="text-dark mt-0 ">Tên công việc</h5></a>
-                            <p class="font-weight-bold ">Tên công ty</p>
-                            <p class="text-info"><i class="fas fa-coins    "></i> Lương: 123</p>
-                            <p><i class="fas fa-map-marker-alt    "></i> Hà Nội</p>
-                            <p ><i class="fas fa-calendar-alt "></i> Hạn chót: 25/09/20</p>
-                        </div>
-                        </div>
-                </button>
-                <button type="button" class="list-group-item list-group-item-action px-0 ">
-                    <div class="media">
-                        <img class="card-img-top col-3" src="http://placehold.it/300x200" alt="Card image cap ">
-                        <div class="media-body text-gray text-left">
-                            <a href=""><h5 class="text-dark mt-0 ">Tên công việc</h5></a>
-                            <p class="font-weight-bold ">Tên công ty</p>
-                            <p class="text-info"><i class="fas fa-coins    "></i> Lương: 123</p>
-                            <p><i class="fas fa-map-marker-alt    "></i> Hà Nội</p>
-                            <p ><i class="fas fa-calendar-alt "></i> Hạn chót: 25/09/20</p>
-                        </div>
-                        </div>
-                </button>
-                <button type="button" class="list-group-item list-group-item-action px-0 ">
-                    <div class="media">
-                        <img class="card-img-top col-3" src="http://placehold.it/300x200" alt="Card image cap ">
-                        <div class="media-body text-gray text-left">
-                            <a href=""><h5 class="text-dark mt-0 ">Tên công việc</h5></a>
-                            <p class="font-weight-bold ">Tên công ty</p>
-                            <p class="text-info"><i class="fas fa-coins    "></i> Lương: 123</p>
-                            <p><i class="fas fa-map-marker-alt    "></i> Hà Nội</p>
-                            <p ><i class="fas fa-calendar-alt "></i> Hạn chót: 25/09/20</p>
-                        </div>
-                        </div>
-                </button>
-                <button type="button" class="list-group-item list-group-item-action px-0 ">
-                    <div class="media">
-                        <img class="card-img-top col-3" src="http://placehold.it/300x200" alt="Card image cap ">
-                        <div class="media-body text-gray text-left">
-                            <a href=""><h5 class="text-dark mt-0 ">Tên công việc</h5></a>
-                            <p class="font-weight-bold ">Tên công ty</p>
-                            <p class="text-info"><i class="fas fa-coins    "></i> Lương: 123</p>
-                            <p><i class="fas fa-map-marker-alt    "></i> Hà Nội</p>
-                            <p ><i class="fas fa-calendar-alt "></i> Hạn chót: 25/09/20</p>
-                        </div>
-                        </div>
-                </button>
-                <button type="button" class="list-group-item list-group-item-action px-0 ">
-                    <div class="media">
-                        <img class="card-img-top col-3" src="http://placehold.it/300x200" alt="Card image cap ">
-                        <div class="media-body text-gray text-left">
-                            <a href=""><h5 class="text-dark mt-0 ">Tên công việc</h5></a>
-                            <p class="font-weight-bold ">Tên công ty</p>
-                            <p class="text-info"><i class="fas fa-coins    "></i> Lương: 123</p>
-                            <p><i class="fas fa-map-marker-alt    "></i> Hà Nội</p>
-                            <p ><i class="fas fa-calendar-alt "></i> Hạn chót: 25/09/20</p>
-                        </div>
-                        </div>
-                </button>
-                <button type="button" class="list-group-item list-group-item-action px-0 ">
-                    <div class="media">
-                        <img class="card-img-top col-3" src="http://placehold.it/300x200" alt="Card image cap ">
-                        <div class="media-body text-gray text-left">
-                            <a href=""><h5 class="text-dark mt-0 ">Tên công việc</h5></a>
-                            <p class="font-weight-bold ">Tên công ty</p>
-                            <p class="text-info"><i class="fas fa-coins    "></i> Lương: 123</p>
-                            <p><i class="fas fa-map-marker-alt    "></i> Hà Nội</p>
-                            <p ><i class="fas fa-calendar-alt "></i> Hạn chót: 25/09/20</p>
-                        </div>
-                        </div>
-                </button>
-                <button type="button" class="list-group-item list-group-item-action px-0 ">
-                    <div class="media">
-                        <img class="card-img-top col-3" src="http://placehold.it/300x200" alt="Card image cap ">
-                        <div class="media-body text-gray text-left">
-                            <a href=""><h5 class="text-dark mt-0 ">Tên công việc</h5></a>
-                            <p class="font-weight-bold ">Tên công ty</p>
-                            <p class="text-info"><i class="fas fa-coins    "></i> Lương: 123</p>
-                            <p><i class="fas fa-map-marker-alt    "></i> Hà Nội</p>
-                            <p ><i class="fas fa-calendar-alt "></i> Hạn chót: 25/09/20</p>
-                        </div>
-                        </div>
-                </button>
-                            
+                <?php } ?>
+          
             </div>
         </div>
-        <div class="d-none d-md-block col-8 detail-job_inf p-0">
+                                                                   
+                                                                              
+        <div class="d-none d-md-block col-8 detail-job_inf p-0" >
             <img src="http://placehold.it/900x200" alt="">
             <div class="row align-items-center  p-3">
                 <div class="col-lg-8">
