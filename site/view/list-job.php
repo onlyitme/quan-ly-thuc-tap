6e7a1b1be107701e26b167ef26f03ccb3299773d
crimeprince4 <73919224+crimeprince4@users.noreply.github.com>
2020-11-21 16:11:10 +0700

    sắp hoàn thiện Ajax load thông tin đăng tuyển

--- site/view/list-job.php
+++ site/view/list-job.php
@@ -8,10 +8,7 @@
             url: "index.php?act=xemdoanhnghiep",
             data: {arr: html},
             success: function(data) {
-              var data =  JSON.parse(data);
-              console.log(data.tieu_de)
-              $("#tieude").html(data.tieu_de)
-              
+              $("#thongtindt").html(data)
             }
           });
         
@@ -54,9 +51,9 @@
                     <div class="media" >
                         <img class="card-img-top col-3" src="../uploads/<?=$thongtindangtuyen['anh']?>" onerror=this.src="http://placehold.it/300x200" alt="Card image cap ">
                         <div class="media-body text-gray text-left">
-                            <a href=""><h5 class="text-dark mt-0 "><?=$thongtinnganh['ten_nganh']?></h5></a>
+                            <a href=""><h5 class="text-dark mt-0 " ><?=$thongtinnganh['ten_nganh']?></h5></a>
                             <p class="font-weight-bold "><?=$thongtindn['ten_dn']?></p>
-                            <p class="text-info"><i class="fas fa-coins    "></i> Lương: <?=$thongtindangtuyen['luong']?></p>
+                            <p class="text-info"><i class="fas fa-coins    "></i> Lương: <?=$thongtindangtuyen['luong']?> $</p>
                             <p><i class="fas fa-map-marker-alt    "></i> <?=$thongtindangtuyen['vi_tri']?></p>
                             <p ><i class="fas fa-calendar-alt "></i> Hạn chót: <?php $today = date("Y-m-d");
                                                                                     $expire = $thongtindangtuyen['thoi_gian'];     
@@ -64,7 +61,7 @@
                                                                                     $expire_time = strtotime($expire);
                                                                                     if ($expire_time < $today_time){
                                                                                         echo '<strong style="color:red">hết hạn nộp</strong>';
-                                                                                    }else echo $expire;
+                                                                                    }else echo  date('d/m/yy',strtotime($expire));
                                                                                     ?></p>
                         </div>
                         </div>
@@ -74,130 +71,8 @@
             </div>
         </div>
                                                                    
-                                                                              
-        <div class="d-none d-md-block col-8 detail-job_inf p-0" >
-            <img src="http://placehold.it/900x200" alt="">
-            <div class="row align-items-center  p-3">
-                <div class="col-lg-8">
-                    <h4 class="mb-0" id='tieude'>Tiêu đề</h4>
-                    <p class="font-weight-bold text-gray mb-0">Tên công ty</p>
-                </div>
-                <div class="col-lg-4 text-right">
-                    <button type="submit" class="btn btn-info ">Nộp đơn ứng tuyển</button>
-                </div>
-            </div>
-            <div class="row p-3">
-                <div class="col-lg-7">
-                    <div class="mb-3">
-                        <h5>Phúc lợi</h5>
-                        <div class="d-flex small">
-                            <div class="col-4">
-                                <p><i class="fas fa-folder-plus  text-primary "></i> Chế độ bảo hiểm</p>
-                                <p><i class="fas fa-graduation-cap   text-primary "></i> Đào tạo</p>
-                            </div>
-                            <div class="col-4">
-                                <p><i class="fas fa-plane  text-primary  "></i> Du lịch</p>
-                                <p><i class="fas fa-sort-numeric-up  text-primary  "></i> Tăng lương</p>
-                            </div>
-                            <div class="col-4">
-                                <p><i class="fas fa-coins  text-primary  "></i> Chế độ thưởng</p>
-                                <p><i class="fas fa-business-time  text-primary  "></i> Nghỉ phép năm</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <h5>Mô tả công việc</h5>
-                        <div id="mota" class="col-12">
-                            <p>Be sure to have your pages set up with </p>
-                            <p> Put it all together and your pages should look like this:</p>
-                            <p>the latest design and development standards. That means using an HTML5 doctype and including a viewport meta tag for proper responsive behaviors. </p>
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <h5>Yêu cầu công việc</h5>
-                        <div id="yeucau" class="col-12">
-                            - Tốt nghiệp Cao đẳng, Đại học chuyên ngành Tài chính - Kế toán. Độ tuổi từ 22 - 28 tuổi. - Có ít nhất 1 năm kinh nghiệm trong lĩnh vực liên quan. - Có khả năng tổng hợp và hướng dẫn, xây dựng kế hoạch để thực hiện nhiệm vụ được phân công - Có thể làm
-                            việc theo nhóm và độc lập. - Sử dụng thành thạo vi tính văn phòng, ERP và phần mềm kế toán - Có khả năng tổ chức công việc, thuyết phục tốt.
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <h5>Quyền lợi</h5>
-                        <div id="quyenloi" class="col-12">
-                            <p> - Thu nhập cạnh tranh theo năng lực làm việc. - Thưởng lương tháng 13 và hiệu quả hoạt động kinh doanh của công ty 2-3 tháng lương. - Nghỉ mát/team-building hàng năm. - Chế độ BHXH, BHYT, BHTN theo quy định của Pháp luật.
-                                - Chế độ ngày nghỉ, lễ tết, được công ty thực hiện theo luật lao động Việt Nam. - Làm việc trong môi trường trẻ năng động, nhiều thử thách và cơ hội phát triển năng lực cá nhân.</p>
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <h5>Thông tin khác</h5>
-                        <div id="quyenloi" class="col-12">
-                            <p>Bằng cấp : Không</p>
-                            <p>Hình thức: Nhân viên thực tập</p>
-                            <p>Thời gian thử việc: 2 tháng</p>
-                            <p>Độ tuổi : 20-28</p>
-                            <p>Thời gian làm việc: Thứ 2 - Thứ 6</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-5">
-                    <div class="mb-4">
-                        <h5>Địa điểm</h5>
-                        <img src="view/images/map.jpg" alt="">
-                    </div>
-                    <div class="mb-4 p-3 shadow">
-                        <h5 class="border-bottom py-3 mb-3">Thông tin tuyển dụng</h5>
-                        <div class="d-flex justify-content-between">
-                            <p class="text-primary">Nơi làm việc</p>
-                            <p class="text-gray">Hồ Chí Minh</p>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <p class="text-primary">Cấp bậc</p>
-                            <p class="text-gray">Nhân viên</p>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <p class="text-primary">Lương</p>
-                            <p class="text-gray">Cạnh tranh</p>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <p class="text-primary">Hết hạn nộp</p>
-                            <p class="text-gray">30/10/2010</p>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <p class="text-primary">Ngành nghề</p>
-                            <p class="text-gray">Thiết kế Website</p>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <p class="text-primary">Kinh Nghiệm</p>
-                            <p class="text-gray">Không</p>
-                        </div>
-                    </div>
-                    <div class="mb-4 p-3 shadow">
-                        <h5 class="border-bottom py-3 mb-3">Giới thiệu công ty</h5>
-                        <div class="d-flex mb-3">
-                            <img src="http://placehold.it/900x200" class="col-4 p-0" alt="logo">
-                            <p class="font-weight-bold col-8">Tên công ty</p>
-                        </div>
-                        <div class="mb-3">
-                            <div class="d-flex ">
-                                <p class="w-25"><i class="fas fa-map-marker-alt"></i></p>
-                                <p class="col-11"> 123 abv, phường 15, q.abv</p>
-                            </div>
-                            <div class="d-flex ">
-                                <p class="w-25"><i class="fas fa-phone-alt"></i></p>
-                                <p class="col-11"> Máđá áđâsd</p>
-                            </div>
-                            <div class="d-flex ">
-                                <p class="w-25"><i class="fas fa-people-carry"></i></p>
-                                <p class="col-11"> 3500 nhân viên</p>
-
-                            </div>
-                            <div class="d-flex">
-                                <p class="w-25"><i class="fab fa-internet-explorer"></i></p>
-                                <p class="col-11"> <a href="">file:///D:/XamPP/htdocs/quan-ly-thuc-tap/site/view/list-job.html</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div class="d-none d-md-block col-8 detail-job_inf p-0" id="thongtindt">
+           <h1>Hãy chọn bài đăng bất kì để coi thông tin</h1>
+        </div>                                                                                                                                            
     </div>
 </div>
