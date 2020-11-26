428da1297c856f96803abefc6b693e30f271ec35
Bảo Kun <69088614+onlyitme@users.noreply.github.com>
2020-11-26 17:41:47 +0700

    update lai trang home

--- site/view/home.php
+++ site/view/home.php
@@ -55,7 +55,7 @@
                                     </div>
                                     <?php if($dem==4){?>
                                     <div class=" col-12 text-center my-3">
-                                        <a type="button" href="index.php?act=listjob&id_nganh=<?=$ds_nganh['id_nganh']?>" class="xemthem col-lg-2 btn btn-inline-block font-weight-bold rounded-lg shadow text-white mb-5"> <i>Xem thêm -></i></a>
+                                        <a type="button" href="index.php?act=listjob&id_nganh=<?=$ds_nganh['id_nganh']?>" class="xemthem col-lg-2 btn btn-secondary btn-inline-block font-weight-bold font-italic rounded-lg  shadow  mb-5"> Xem thêm -></a>
                                     </div>
                                     <?php } ?>
                                     <?php  $dem++;}?>
