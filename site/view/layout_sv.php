c9c9dfa0cf01acf95954f452de3947b93efffb4f
crimeprince4 <73919224+crimeprince4@users.noreply.github.com>
2020-11-27 11:20:18 +0700

    xóa model show phiếu đk in

--- site/view/layout_sv.php
+++ site/view/layout_sv.php
@@ -13,18 +13,7 @@
                                         Quản lý thông tin
                                     </a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="file:///C:/Users/ASUS/Documents/GitHub/quan-ly-thuc-tap/site/view/sv2-dsdonxv.html">
-                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-card-list" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd" d="M14.5 3h-13a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
-                                            <path fill-rule="evenodd" d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5z"/>
-                                            <circle cx="3.5" cy="5.5" r=".5"/>
-                                            <circle cx="3.5" cy="8" r=".5"/>
-                                            <circle cx="3.5" cy="10.5" r=".5"/>
-                                        </svg>
-                                        Danh sách đơn xin thực tập
-                                    </a>
-                                </li>
+                               
                                 <li class="nav-item">
                                     <a class="nav-link" href="file:///C:/Users/ASUS/Documents/GitHub/quan-ly-thuc-tap/site/view/sv3-qlhoso.html">
                                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-earmark-text" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -44,128 +33,17 @@
                                     </a>
                                 </li>
                                 <li class="h-header" style="color: white;">VIỆC LÀM & HỖ TRỢ</li>
-                                <li class="nav-item" data-toggle="modal" data-target=".bs-example-modal-lg">
-                                    <a class="nav-link" data-toggle="modal" data-target="#myModal" href="file:///C:/Users/ASUS/Documents/GitHub/quan-ly-thuc-tap/site/view/sv5-tbvieclam.html">
-                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bell" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2z"/>
-                                            <path fill-rule="evenodd" d="M8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="file:///C:/Users/ASUS/Documents/GitHub/quan-ly-thuc-tap/site/view/sv2-dsdonxv.html">
+                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-card-list" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" d="M14.5 3h-13a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+                                            <path fill-rule="evenodd" d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5z"/>
+                                            <circle cx="3.5" cy="5.5" r=".5"/>
+                                            <circle cx="3.5" cy="8" r=".5"/>
+                                            <circle cx="3.5" cy="10.5" r=".5"/>
                                         </svg>
-                                        Thông báo việc làm
+                                        Danh sách đơn xin thực tập
                                     </a>
-                                    <div class="row">
-                                        <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargemoalLabel">
-                                            <div class="modal-dialog modal-lg" role="document">
-                                                <div class="modal-content modal-content1">
-                                                    <div class="modal-body">
-                                                        <div class="media col-sm-12 sv-12">
-                                                            <img src="images/logofpt.png" alt="John Doe" class="mr-3 mt-3 mb-3 rounded-circle align-self-center" style="width:80px;border: 1px black solid;">
-                                                                <div class="media-body col-sm-11">
-                                                                    <a href="#">
-                                                                        <h5 class="media-heading mt-3">CÔNG TY CP FPT</h5>
-                                                                    </a>
-                                                                    
-                                                                    <p class="name">NGÀNH NGHỀ: Hoạt động thông tin khác...</p>
-                                                                    <p style="float:left;width: 60%;"class="mb-3">Ngày hết hạn : 16/12/2020</p>     
-                                                                    <div style="color: rgb(255, 153, 0);"><span style="margin-top: -10px;" ></span>
-                                                                        Không được duyệt
-                                                                    </div>
-                                                                    
-                                                                </div>
-                                                                <div style="margin-left: 10px;" class="col-sm-1 ">
-                                                                    <a href="#">
-                                                                        <i class="fa fa-close" style="font-size:24px;color:rgba(5, 5, 5, 0.466);margin-left: 50px;margin-top: 5px;"></i>
-                                                                    </a>
-                                                                    
-                                                                </div>
-                                                        
-                                                        </div>
-                                                        <div class="col-sm-12 ">
-                                                            <div class="col-sm-2 mr-center">
-                                                                <hr style="background: rgb(110, 110, 110);">
-                                                            </div>     
-                                                        </div>
-                                                        <div class="media col-sm-12 sv-12">
-                                                            <img src="images/lgvinamilk.jpg" alt="John Doe" class="mr-3 mt-3 mb-3 rounded-circle align-self-center" style="width:80px;border: 1px black solid;">
-                                                                <div class="media-body col-sm-11">
-                                                                    <a href="#">
-                                                                        <h5 class="media-heading mt-3">CÔNG TY CP SỮA VIỆT NAM</h5>
-                                                                    </a>
-                                                                    
-                                                                    <p class="name">NGÀNH NGHỀ: Sản xuất, kinh doanh sữa đạt chuẩn...</p>
-                                                                    <p style="float:left;width: 60%;"class="mb-3">Ngày hết hạn : 16/12/2020</p>     
-                                                                    <div style="color: rgb(81, 255, 0);"><span style="margin-top: -10px;" ></span>
-                                                                        Đã phê duyệt
-                                                                    </div>
-                                                                    
-                                                                </div>
-                                                                <div style="margin-left: 10px;" class="col-sm-1 ">
-                                                                    <a href="#">
-                                                                        <i class="fa fa-close" style="font-size:24px;color:rgba(5, 5, 5, 0.466);margin-left: 50px;margin-top: 5px;"></i>
-                                                                    </a>
-                                                                    
-                                                                </div>
-                                                        
-                                                        </div>
-                                                        <div class="col-sm-12 ">
-                                                            <div class="col-sm-2 mr-center">
-                                                                <hr style="background: rgb(110, 110, 110);">
-                                                            </div>     
-                                                        </div>
-                                                        <div class="media col-sm-12 sv-12">
-                                                            <img src="images/lgvinfast.jpg" alt="John Doe" class="mr-3 mt-3 mb-3 rounded-circle align-self-center" style="width:80px;border: 1px black solid;">
-                                                                <div class="media-body col-sm-11">
-                                                                    <a href="#">
-                                                                        <h5 class="media-heading mt-3"> CÔNG TY CP VINFAST </h5>
-                                                                    </a>
-                                                                    
-                                                                    <p class="name">NGÀNH NGHỀ: Sản xuất, kinh doanh ô tô thế hệ mới...</p>
-                                                                    <p style="float:left;width: 60%;"class="mb-3">Ngày hết hạn : 18/12/2020</p>     
-                                                                    <div style="color: rgb(255, 153, 0);"><span style="margin-top: -10px;" ></span>
-                                                                        Không được duyệt
-                                                                    </div>
-                                                                    
-                                                                </div>
-                                                                <div style="margin-left: 10px;" class="col-sm-1 ">
-                                                                    <a href="#">
-                                                                        <i class="fa fa-close" style="font-size:24px;color:rgba(5, 5, 5, 0.466);margin-left: 50px;margin-top: 5px;"></i>
-                                                                    </a>
-                                                                    
-                                                                </div>
-                                                        
-                                                        </div>
-                                                        <div class="col-sm-12 ">
-                                                            <div class="col-sm-2 mr-center">
-                                                                <hr style="background: rgb(110, 110, 110);">
-                                                            </div>     
-                                                        </div>
-                                                        <div class="media col-sm-12 sv-12">
-                                                            <img src="images/olam.jpg" alt="John Doe" class="mr-3 mt-3 mb-3 rounded-circle align-self-center" style="width:80px;border: 1px black solid;">
-                                                                <div class="media-body col-sm-11">
-                                                                    <a href="#">
-                                                                        <h5 class="media-heading mt-3">CÔNG TY TNHH OLAM VIỆT NAM </h5>
-                                                                    </a>
-                                                                    
-                                                                    <p class="name">NGÀNH NGHỀ: Sản xuất, chế biến và kinh doanh...</p>
-                                                                    <p style="float:left;width: 60%;"class="mb-3">Ngày hết hạn : 19/12/2020</p>     
-                                                                    <div style="color: rgb(255, 153, 0);"><span style="margin-top: -10px;" ></span>
-                                                                        Không được duyệt
-                                                                    </div>
-                                                                    
-                                                                </div>
-                                                                <div style="margin-left: 10px;" class="col-sm-1 ">
-                                                                    <a href="#">
-                                                                        <i class="fa fa-close" style="font-size:24px;color:rgba(5, 5, 5, 0.466);margin-left: 50px;margin-top: 5px;"></i>
-                                                                    </a>
-                                                                    
-                                                                </div>
-                                                        
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-        
-                                        </div>
-                                    </div>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">
