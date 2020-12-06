d2cc28945ae3632c126ccf1b68e12ccbb5527b30
nghiattps13085 <74232539+nghiattps13085@users.noreply.github.com>
2020-12-06 17:19:05 +0700

    Nghia

--- site/view/danh_sach_don.php
+++ site/view/danh_sach_don.php
@@ -23,16 +23,16 @@
                     xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd"
                         d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                </svg>:
-                <a style="font-size: 10pt;" href="#"><?=$thong_tin_dn['dia_chi']?></a>
+                </svg>
+                <a class="text-dark" style="font-size: 10pt;cursor:auto;" href="#"><?=$thong_tin_dn['dia_chi']?></a>
             </p>
             <p style="margin-bottom:-1px;" class="mb-2">
                 <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-telephone-fill" fill="currentColor"
                     xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd"
                         d="M2.267.98a1.636 1.636 0 0 1 2.448.152l1.681 2.162c.309.396.418.913.296 1.4l-.513 2.053a.636.636 0 0 0 .167.604L8.65 9.654a.636.636 0 0 0 .604.167l2.052-.513a1.636 1.636 0 0 1 1.401.296l2.162 1.681c.777.604.849 1.753.153 2.448l-.97.97c-.693.693-1.73.998-2.697.658a17.47 17.47 0 0 1-6.571-4.144A17.47 17.47 0 0 1 .639 4.646c-.34-.967-.035-2.004.658-2.698l.97-.969z" />
-                </svg>:
-                <a style="font-size: 10pt;" href="#"><?=$thong_tin_dn['sdt']?> </a>– <a style="font-size: 10pt;"
+                </svg>
+                <a  class="text-dark" style="font-size: 10pt;cursor:auto;" href="#"><?=$thong_tin_dn['sdt']?> </a>– <a class="text-dark" style="font-size: 10pt;cursor:auto;"
                     href="#"><?=$thong_tin_dn['sdt_ban']?></a>
             </p>
         </div>
