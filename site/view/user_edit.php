925162811ccecfc24dfff7c3b931763ce7469b97
nghiattps13085 <74232539+nghiattps13085@users.noreply.github.com>
2020-12-07 00:29:19 +0700

    Nghia

--- site/view/user_edit.php
+++ site/view/user_edit.php
@@ -1,11 +1,11 @@
       
-                         <form  action="index.php?ctrl=sinh_vien&act=user_update" method="post" class="h-100 Ng" id="capnhapuser" enctype="multipart/form-data">
-                         <div class="bgr "></div>
+                         <form  action="index.php?ctrl=sinh_vien&act=user_update" method="post" class="Ng" id="capnhapuser" enctype="multipart/form-data">
+                         <div class="bgr rounded shadow "></div>
                             <div class="media boder p-3 sv8-12 "style="border-radius: 5px;">
                             
                                 <div class="row col-sm-12">
                                     <div  class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 mb-3">
-                                    <h5 class="mb-3" style="padding: 0 5px;font-weight: 600;">THAY ĐỔI MẬT KHẨU </h5>         
+                                    <h3 class="mb-3" style="padding: 0 5px;font-weight: 600;">THAY ĐỔI MẬT KHẨU </h3>         
                                     </div>                                   
 
                                     <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 ml-4"style="text-align: right;">
@@ -30,18 +30,9 @@
                                     <div class="col-12 text-center mt-4">
                                        
                                             <button class="buttoncapnhat ml-4" type="submit" >
-                                            <svg style="color: white;" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-journal-bookmark mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
-                                                <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
-                                                <path fill-rule="evenodd" d="M6 8V1h1v6.117L8.743 6.07a.5.5 0 0 1 .514 0L11 7.117V1h1v7a.5.5 0 0 1-.757.429L9 7.083 6.757 8.43A.5.5 0 0 1 6 8z"/>
-                                            </svg>
                                             Cập nhật
                                         </button>
                                         <button class="btn btn-secondary 2 ml-2" type="button" > 
-                                            <svg style="color:white;" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-circle mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                                <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                                            </svg>
                                             Hủy
                                         </button>
                                         
