d2938314ea2ca631d26ef5076853c355a279ac71
Hoang Nguyen <hoangdean01@gmail.com>
2020-12-07 01:39:03 +0700

    hoang

--- site/view/ttcn_index.php
+++ site/view/ttcn_index.php
@@ -1,66 +1,68 @@
-<div class="shadow ">
+<div class="h-tt-left rounded shadow-sm">
+    <div class="h-tt-left-header rounded">
+        <div class="h-tt-left-img">
+            <img src="../uploads/<?= $ttsv['anh'] ?>" onerror=this.src="view/images/student.png" class="avt-ttsv" id="avt-sv" alt="avatar">
+            <div class="h-tt-left-img-txt">
+                <label for="file-upload-SV" class="h-custom-file-upload">
+                    <i class="far fa-camera"></i>
+                </label>
+                <input id="file-upload-SV" type="file" name="file">
+            </div>
+        </div>
+        <div class="h-tt-left-text">
+            <div class="h-tttk-trangthai">
+                <p class=""> <i class="fas fa-medal"></i></p>
+            </div>
+            <span class="text-uppercase ">
+                <h2> <?= $ttsv['ho_ten'] ?> </h2>
+            </span>
+            <span class="text-dark">Thực tập sinh | </span>
+            <span class="text-dark font-weight-lighter font-italic"> &lpar; Đang thực tập &rpar; <i class="fa fa-circle text-success"></i></span>
+            <hr class="h-bv-hr">
+
+            <div class="h-ttcn text-left">
+                <form action="">
+                    <label class="sr-only" for=""></label>
+                    <div class="input-group mr-sm-2">
+                        <div class="input-group-prepend h-tttk-input">
+                            <label class="col-form-label">Tên tài khoản:</label>
+                        </div>
+                        <input type="text" class="form-control col-md-3 border-0" readonly id="" value="<?= $tttk['user'] ?>">
+                    </div>
+
+                    <label class="sr-only" for=""></label>
+                    <div class="input-group mr-sm-2">
+                        <div class="input-group-prepend h-tttk-input">
+                            <div class="col-form-label">Mật khẩu:</div>
+                        </div>
+                        <input type="password" class="form-control col-md-3 border-0" readonly id="" value="<?= $tttk['pass'] ?>">
+                    </div>
+
+                    <label class="sr-only" for=""></label>
+                    <div class="input-group mr-sm-2">
+                        <div class="input-group-prepend h-tttk-input">
+                            <div class="col-form-label">Email:</div>
+                        </div>
+                        <input type="text" class="form-control col-md-4 border-0" readonly id="" value="<?= $tttk['email'] ?>">
+                    </div>
+
+                </form>
+            </div>
+            <div class="h-tt-social">
+                <span class="h-social">
+                    <a href="https://www.facebook.com/profile.php?=75816879" target="_blank"><i class="fa fa-facebook-square"></i></a>
+                    <a href=""><i class="fa fa-twitter-square"></i></a>
+                    <a href=""><i class="fa fa-github-square"></i></a>
+                    <a href=""><i class="fa fa-share-square"></i></a>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="w-100 h-100 p-3 shadow rounded">
     <div class="h-big-box mt-3">
         <form action="">
-            <div class="h-tt-left rounded shadow-sm">
-                <div class="h-tt-left-header rounded">
-                    <div class="h-tt-left-img">
-                        <img src="../uploads/<?=$ttsv['anh']?>" onerror=this.src="view/images/student.png" class="avt-ttsv" id="avt-sv" alt="avatar">
-                        <div class="h-tt-left-img-txt">
-                            <label for="file-upload-SV" class="h-custom-file-upload">
-                                <i class="far fa-camera"></i>
-                            </label>
-                            <input id="file-upload-SV" type="file" name="file">
-                        </div>
-                    </div>
-                    <div class="h-tt-left-text">
-                        <div class="h-tttk-trangthai">
-                            <p class="text-success"> <i class="fas fa-check-circle    "></i> </p>
-                        </div>
-                        <span class="text-uppercase ">
-                            <h2> <?= $ttsv['ho_ten'] ?> </h2>
-                        </span>
-                        <span class="text-dark font-weight-light"><i class="fas fa-circle text-success"></i> Thực tập sinh </span>
-                        <hr class="h-bv-hr">
-
-                        <div class="h-ttcn text-left">
-                            <form action="">
-                                <label class="sr-only" for=""></label>
-                                <div class="input-group mr-sm-2">
-                                    <div class="input-group-prepend h-tttk-input">
-                                        <label class="col-form-label">Tên tài khoản:</label>
-                                    </div>
-                                    <input type="text" class="form-control col-md-3 border-0" readonly id="" value="<?= $tttk['user'] ?>">
-                                </div>
-
-                                <label class="sr-only" for=""></label>
-                                <div class="input-group mr-sm-2">
-                                    <div class="input-group-prepend h-tttk-input">
-                                        <div class="col-form-label">Mật khẩu:</div>
-                                    </div>
-                                    <input type="password" class="form-control col-md-3 border-0" readonly id="" value="<?= $tttk['pass'] ?>">
-                                </div>
-
-                                <label class="sr-only" for=""></label>
-                                <div class="input-group mr-sm-2">
-                                    <div class="input-group-prepend h-tttk-input">
-                                        <div class="col-form-label">Email:</div>
-                                    </div>
-                                    <input type="text" class="form-control col-md-4 border-0" readonly id="" value="<?= $tttk['email'] ?>">
-                                </div>
-
-                            </form>
-                        </div>
-                        <div class="h-tt-social">
-                            <span class="h-social">
-                                <a href="#"><i class="fa fa-facebook-square"></i></a>
-                                <a href="#"><i class="fa fa-reddit-square"></i></a>
-                                <a href="#"><i class="fa fa-twitter-square"></i></a>
-                                <a href="#"><i class="fa fa-github-square"></i></a>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
             <div class="h-box-ttsv rounded-sm mt-4">
                 <div class="h-ttsv-right w-100">
